6dfedd38044ccbf897431087a077c928254a5007
Peter Tatrai <peter@tatrai.sk>
2026-05-13 12:02:51 +0000

    chore(example): modernize pcp example client and build script

--- example/pcp-example.sh
+++ example/pcp-example.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-gcc pcp-example.c -o pcp-example -O0 -g -lpcpnatpmp -I ../lib/include
+set -eu
+
+if command -v pkg-config >/dev/null 2>&1 && pkg-config --exists libpcpnatpmp; then
+    if cc pcp-example.c -o pcp-example -O0 -g $(pkg-config --cflags --libs libpcpnatpmp) 2>/dev/null; then
+        exit 0
+    fi
+fi
+
+cc pcp-example.c -o pcp-example -O0 -g -I ../lib/include -L ../lib/.libs -lpcpnatpmp
